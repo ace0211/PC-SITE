72baf39960c7bc6e87245f9048161029a92b8b69
주예지 <165743668+ace0211@users.noreply.github.com>
2025-03-23 21:19:14 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
 import Detail from "./pages/PartDetail";
+import Recommend from "./pages/Recommend";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/category/:category" element={<Category />} />
         <Route path="/detail/:category/:id" element={<Detail />} />
+        <Route path="/ai-recommend" element={<Recommend />} /> {/* ✅ 이 부분 추가 */}
       </Routes>
     </Router>
   );
